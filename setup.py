d17ecaf8901582948e8832e2cdc287f1aa3a7036
James Addison <james@reciperadar.com>
2020-12-17 16:31:17 +0000

    Remove legacy test runner

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
-import re
-
 from setuptools import setup
-from setuptools.command.test import test
-
-
-# use pytest instead
-def run_tests(self):
-    pyc = re.compile(r'\.pyc|\$py\.class')
-    test_file = pyc.sub('.py', __import__(self.test_suite).__file__)
-    raise SystemExit(__import__('pytest').main(['-vv', test_file]))
-test.run_tests = run_tests
 
 
 setup(
