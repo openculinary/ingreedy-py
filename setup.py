1cade12d83b50d3c313a0c7d8438b337c9eeed88
James Addison <james@reciperadar.com>
2020-12-17 14:17:28 +0000

    Release 1.3.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ test.run_tests = run_tests
 setup(
     name='ingreedypy',
     py_modules=['ingreedypy'],
-    version='1.3.1',
+    version='1.3.2',
     description='ingreedy-py parses recipe ingredient lines into a object',
     author='Scott Cooper',
     author_email='scttcper@gmail.com',
