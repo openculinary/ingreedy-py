9108add7219d3d70ff0aab86c13cd4077cda6619
James Addison <james@reciperadar.com>
2020-12-17 16:32:21 +0000

    Add long description for package

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 
 setup(
     name='ingreedypy',
     py_modules=['ingreedypy'],
     version='1.3.2',
     description='ingreedy-py parses recipe ingredient lines into a object',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Scott Cooper',
     author_email='scttcper@gmail.com',
     url='https://github.com/openculinary/ingreedy-py',
